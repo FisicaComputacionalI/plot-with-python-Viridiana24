65ad537d03bf1e7d2f8b019652c96c13da1cc8b0
Viridiana <31377504+Viridiana24@users.noreply.github.com>
2017-08-30 13:47:47 -0500

    Add files via upload
    Profesora no se ve mi gràfica

--- temp.py
+++ temp.py
@@ -3,9 +3,12 @@ import matplotlib.pyplot as plt
 #Construimos el objeto plt introduciendo valores que se grafican contra el numero de entrada. Por ejemplo si queremos graficar el crecimiento de un material por dia en centimetros. 
 plt.plot([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22])
 #Con esta isntruccion colocamos un titulo al eje Y de la grafica. 
-plt.ylabel('Numero de primos y hermanos en mis anios de vida')
+plt.ylabel('Numero de primos y hermanos')
+plt.xlabel('anios de vida')
+plt.grid(true)
 #Con esta isntruccion guardamos la imagen con el formato que queramos.
 plt.savefig('temp.png')
 #Con esta instruccion mostramos la figura. 
 plt.show()
+
 
